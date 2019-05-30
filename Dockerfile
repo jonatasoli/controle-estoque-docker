8a913eaf40078682d3cf675a49013cd8fdedf1dc
Jônatas Oliveira <contato@jonatasoliveira.me>
2019-05-29 22:52:21 -0300

    add postgres docker image

--- Dockerfile
+++ Dockerfile
@@ -6,14 +6,22 @@ ENV PYTHONUNBUFFERED 1
 ARG SECRET_KEY_ARG
 ARG ALLOWED_HOSTS_ARG
 ARG DEBUG_ARG
+ARG DB_HOST
+ARG DB_NAME
+ARG DB_USER
+ARG DB_PASS
 
 ENV SECRET_KEY=${SECRET_KEY_ARG}
 ENV ALLOWED_HOSTS=${ALLOWED_HOSTS_ARG}
 ENV DEBUG=${DEBUG_ARG}
+ENV DATABASE_URL=postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:5432/${DB_NAME}
 
 COPY ./requirements.txt /requirements.txt
-
-RUN pip install -r requirements.txt
+RUN apk add --update --no-cache postgresql-client
+RUN apk add --update --no-cache --virtual .tmp-build-deps \
+      gcc libc-dev linux-headers postgresql-dev
+RUN pip install -r /requirements.txt
+RUN apk del .tmp-build-deps
 
 RUN mkdir /src
 WORKDIR /src
--- src/controle_estoque/settings.py
+++ src/controle_estoque/settings.py
@@ -1,5 +1,6 @@
 import os
 from decouple import config, Csv
+from dj_database_url import parse as dburl
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -56,13 +57,10 @@ WSGI_APPLICATION = 'controle_estoque.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
